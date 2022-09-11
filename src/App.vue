fb36b520500226143b11f9e616d97292db2e764b
azizdevfull <aziz16110904@gmail.com>
2022-09-11 18:33:09 +0500

    my first vue js practice

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <comment-form @addComment="createComment" />
+        <comment-list :comments="comments" />
+    </div>
+</template>
+
+<script>
+    import CommentForm from './components/CommentForm';
+    import CommentList from './components/CommentList';
+
+export default {
+    components: {
+        CommentForm,
+        CommentList,
+    },
+    data() {
+        return {
+            comments: [
+                { id: 1, name: "Aziz", email: "aziz@gmail.com", content: "Lorem ipsum dolor sit" },
+                { id: 2, name: "Murod", email: "murod@gmail.com", content: "Lorem ipsum dolor sit" },
+                { id: 3, name: "Aziza", email: "aziza@gmail.com", content: "Lorem ipsum dolor sit" },
+
+
+            ],
+        }
+    },
+    methods: {
+        createComment(comment){
+            this.comments.push(comment);
+        }
+    },
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/CommentForm.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <div class="w-75 mx-auto">
+            <h3>Add Comment</h3>
+            <form @submit.prevent>
+                <input v-model="comment.name" placeholder="Name"
+                    class="form-control" />
+                <input v-model="comment.email" placeholder="Email"
+                    class="form-control my-3" />
+                <input v-model="comment.content" placeholder="Content"
+                    class="form-control" />
+                <div style="text-align: right;">
+                    <button @click="createComment" class="btn btn-primary mt-3 ml-auto a">Add Comments</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            comment: {
+            name: "",
+            email: "",
+            content: ""
+            }
+        }
+    },
+    methods: {
+        createComment(){
+            this.comment.id = Date.now();
+            this.$emit('addComment', this.comment);
+            this.comment = {
+                name: "",
+                email: "",
+                content: ""
+            }
+        }
+    }
+}
+</script>
+
+<style>
+    
+</style>
